2c3096bc7bc6b48b49c2a5178901f70a9b5ecac1
Benatovich <sambenatovich@gmail.com>
2022-03-23 15:23:56 -0400

    completed exercise 1

--- exercise-1/exercise-1.sql
+++ exercise-1/exercise-1.sql
@@ -3,3 +3,24 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+SELECT
+	title,
+    COUNT (*)
+FROM 
+	rental
+INNER JOIN
+	inventory
+ON
+	rental.inventory_id = inventory.inventory_id
+INNER JOIN
+	film
+ON
+	film.film_id = inventory.film_id
+GROUP BY 
+	film.film_id
+ORDER BY
+	COUNT DESC
+LIMIT 10
+
+
+
